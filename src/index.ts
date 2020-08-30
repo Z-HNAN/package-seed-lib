db52dec6c249068919e4270d3398b70fc238901b
z-hnan <zhn68033@163.com>
2020-08-31 02:14:42 +0800

    feat: 使用es6实例

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import moduleA from './moduleA';
 
 export default function hello(name: string): string {
-  moduleA({ name: '1' });
+  const res = Promise.resolve(() => {
+    console.log('use es6');
+  });
+  console.log(res);
+
   return `hello ${name}`;
 }
